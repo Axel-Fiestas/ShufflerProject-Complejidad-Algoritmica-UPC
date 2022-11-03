feb5fa423eb9518e38696cbebd358b558734901f
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-11-02 23:06:56 -0500

    Beta Version Funcionality

--- FunctionsGUI.py
+++ FunctionsGUI.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from ReadCsvToNewCsv import *
 from PIL import Image, ImageTk
+import networkx as nx
+import matplotlib.pyplot as plt
+from menu_Program import *
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
 
 def showSongs(root,img_boton,printNameSong):
 
@@ -28,3 +33,95 @@ def showSongs(root,img_boton,printNameSong):
         #    boton.configure(image=img_boton_check, state=DISABLED)
         #botonSeleccionador.grid(row=rowIterador,column=1)
 
+def extracSong(song,playlist_tracks):
+    for track in playlist_tracks:
+        if song==track.name:
+            return track
+
+def pressButtonArtist(songName):
+
+    grafo = nx.Graph()  # Creamos el grafo
+
+    song=extracSong(songName,playlist_tracks)
+
+    generateNodesCaseByArtist(grafo, song, playlist_tracks)  # Generamos los nodos de canciones
+    # Se emplearán los Conjuntos disjuntos , para este caso
+    listaId = []  # Lista de Ids
+    diccionario = {}  # Diccionario que tendra el Id de cada diccionario
+    diccionario_objetos = {}  # Diccionario que tendra el objeto en si para poder acceder a sus variables
+    # Definimos la lista de ids (que seran los artistas) y lo demas que servirá como conexión para verificar si una canción
+    # comparte conexión (comparten mismo artista) con otra
+    for i in range(len(playlist_tracks)):
+        diccionario[playlist_tracks[i].name] = i  # Paranoid : 1
+        diccionario_objetos[i] = playlist_tracks[i]  # ObjetoCancion(Paranoid) : 1
+        listaId.append(playlist_tracks[i].artist)  # lista[1] = Black Sabbath
+    grafo.add_node(
+        song.artist)  # En el caso de filtración por artistas SOLO se añade el artista de la canción elegida como un nodo
+    # print(diccionario)
+    # print(diccionario_objetos)
+    # print(listaId)
+    # Generar las conexiones de aristas y mostrar el grafo
+    for i in range(len(listaId)):
+        # i-> valor
+        # listaid[i] -> id
+        valor = diccionario_objetos[i].artist
+        if (valor == listaId[i] and valor == song.artist):
+            grafo.add_edge(diccionario_objetos[i].name, listaId[i])
+    songs_of_group = list(grafo.adj[song.artist])
+    print(f"Songs Of artist ''{song.artist}'' in the playlist are: ")
+    for x in songs_of_group:
+        print(x)
+    nx.draw(grafo, pos=nx.spring_layout(grafo), with_labels=True)
+    plt.show()
+
+def pressButtonSeems(songName):
+
+    grafo = nx.Graph()  # Creamos el grafo
+    song=extracSong(songName,playlist_tracks)# Extraemos la cancion
+
+    grafo.add_node(song.name)#Añadimos el nodo de la cancion
+
+    numberOfPlaylist = int(askstring('Quantity', 'How many songs do you need?'))
+    #showinfo('Hello!', 'Hi, {}'.format(name))
+
+    generateNodesCaseBySeems(grafo, song, playlist_tracks,numberOfPlaylist)
+    #Realizamos y mostramos el grafo de las canciones con su respectiva similitud
+    elarge = [(u, v) for (u, v, d) in grafo.edges(data=True) if d["weight"] > 5]
+    esmall = [(u, v) for (u, v, d) in grafo.edges(data=True) if d["weight"] <= 5]
+    pos = nx.spring_layout(grafo, seed=4)
+    nx.draw_networkx_nodes(grafo, pos, node_size=700)
+    nx.draw_networkx_edges(grafo, pos, edgelist=elarge, width=3)
+    nx.draw_networkx_edges(
+        grafo, pos, edgelist=esmall, width=3, alpha=0.5, edge_color="b", style="dashed"
+    )
+    nx.draw_networkx_labels(grafo, pos, font_size=20, font_family="sans-serif")
+    edge_labels = nx.get_edge_attributes(grafo, "weight")
+    nx.draw_networkx_edge_labels(grafo, pos, edge_labels)
+    ax = plt.gca()
+    ax.margins(0.08)
+    plt.axis("off")
+    plt.tight_layout()
+
+
+    songs_of_seems = list(grafo.adj[song.name])
+    print(f"Songs Of Seems that ''{song.name}'' in the playlist are: ")
+    for x in songs_of_seems:
+       print(x)
+
+    plt.show()
+
+def pressButtonPopularity(songName):
+
+    grafo = nx.Graph()  # Creamos el grafo
+    song=extracSong(songName,playlist_tracks)# Extraemos la cancion
+
+    generateNodesCaseByPopularity(grafo, song, playlist_tracks)
+    conectionByPopularity(playlist_tracks,grafo,song)
+
+    songs_of_group=list(grafo.adj[song.name])
+    print(f"Songs Of same popularity in the playlist are: ")
+    for x in songs_of_group:
+       print(x)
+
+    nx.draw(grafo, pos=nx.spring_layout(grafo), with_labels=True)
+    plt.show()
--- GUIProject.py
+++ GUIProject.py
@@ -2,9 +2,12 @@
 import tkinter
 from tkinter import *
 from ReadCsvToNewCsv import *
+from  Function import *
 from PIL import Image, ImageTk
 from menu_Program import *
-import networkx as nx
+from FunctionsGUI import *
+
+
 
 #logoChooseImage=Image.open('images/choose_logo.png')
 #checkImage=Image.open('images/check_icon.png')
@@ -26,8 +29,6 @@ def printTheMessage(message):
     new_label.grid(row=2,column=2)
 
 
-
-
 titleApp=tkinter.Label(root,text="Recomend Playlist Project").grid(row=0,column=0)
 secondaryTitle=tkinter.Label(root,text="Write a song of this playlist!",bg="yellow").grid(row=1,column=0)
 
@@ -38,9 +39,26 @@ e.grid(row=2,column=0)
 e.get()
 
 
+buttonArtist=Button(root,text="Filter By Group",command=lambda:pressButtonArtist(e.get())).grid(row=3,column=0)
+buttonSeems=Button(root,text="Filter By Seems", command=lambda :pressButtonSeems(e.get())).grid(row=3,column=1)
+buttonPopularity=Button(root,text="Filter By Popularity",command=lambda: pressButtonPopularity(e.get())).grid(row=3,column=2)
+
+
+#buttonExample=Button(root,text="ButtonExampleCheck",command=lambda: pressButon(e.get())).grid(row=3,column=0)
 
 
 
+def pressButon(song):
+    labelWarning = tkinter.Label(root, text="Something was wrong", bg="red")
+    if(not checkSong(song,playlist_tracks)):
+        labelWarning.grid(row=2,column=1)
+        return False
+
+    labelWarning.grid_remove()
+
+    print("Hola!")
+
+#Check the song when you are pressing the botton
 
 
 
